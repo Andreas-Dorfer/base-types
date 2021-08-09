316d3f3cc3a8dc1fe5db2e273cc06e2e997b957a
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-09 11:02:59 +0200

    Common

---
+++ src/AD.BaseTypes/Common.cs
@@ -0,0 +1,17 @@
+﻿namespace AD.BaseTypes
+{
+    /// <summary>
+    /// A GUID that's never empty.
+    /// </summary>
+    [NonEmptyGuid] public partial record NonEmptyGuid;
+
+    /// <summary>
+    /// A string that's never null or empty.
+    /// </summary>
+    [NonEmptyString] public partial record NonEmptySting;
+
+    /// <summary>
+    /// A decimal that's never negative.
+    /// </summary>
+    [PositiveDecimal] public partial record PositiveDecimal;
+}
