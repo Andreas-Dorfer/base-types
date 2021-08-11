2704b2d14fee32abed864bb12cbaf14815f08935
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-11 16:33:43 +0200

    json

---
+++ src/AD.BaseTypes/Json/BaseTypeJsonConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace AD.BaseTypes.Json
+{
+    public abstract class BaseTypeJsonConverter<TBaseType, TWrapped> : JsonConverter<TBaseType> where TBaseType : IValue<TWrapped>
+    {
+        public BaseTypeJsonConverter(Func<TWrapped, TBaseType> creator)
+        {
+            Creator = creator;
+        }
+
+        protected Func<TWrapped, TBaseType> Creator { get; }
+    }
+}
---
+++ src/AD.BaseTypes/Json/IntJsonConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.Json;
+
+namespace AD.BaseTypes.Json
+{
+    public class IntJsonConverter<TBaseType> : BaseTypeJsonConverter<TBaseType, int> where TBaseType : class, IValue<int>
+    {
+        public IntJsonConverter(Func<int, TBaseType> creator) : base(creator)
+        { }
+
+        public override void Write(Utf8JsonWriter writer, TBaseType value, JsonSerializerOptions options) =>
+            writer.WriteNumberValue(value.Value);
+
+        public override TBaseType? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+            reader.TryGetInt32(out var value) ? Creator(value) : null;
+    }
+}
