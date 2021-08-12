cabb5254afe5608533f7dba451398564d2e8f295
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-12 10:15:58 +0200

    TestApp

---
+++ src/TestApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using AD.BaseTypes;
+using System;
+
+namespace TestApp
+{
+    [Int] partial record MyInt;
+    [MaxLength(4)] partial record MyText;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
