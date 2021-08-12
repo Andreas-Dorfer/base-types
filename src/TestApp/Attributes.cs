cd7c3ffcee08536eb5b3709a30fed7308030d687
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-12 15:44:48 +0200

    attribute examples

---
+++ src/TestApp/Attributes.cs
@@ -0,0 +1,29 @@
+﻿using AD.BaseTypes;
+using System;
+
+namespace TestApp
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    class WeekendAttribute : Attribute, IBaseTypeValidation<DateTime>
+    {
+        public void Validate(DateTime value)
+        {
+            if (value.DayOfWeek != DayOfWeek.Saturday && value.DayOfWeek != DayOfWeek.Sunday)
+                throw new ArgumentOutOfRangeException(nameof(value), value, "must be a Saturday or Sunday");
+        }
+    }
+
+    [Weekend] partial record SomeWeekend;
+
+    [AttributeUsage(AttributeTargets.Class)]
+    class The90sAttribute : Attribute, IBaseTypeValidation<DateTime>
+    {
+        public void Validate(DateTime value)
+        {
+            if (value.Year < 1990 || value.Year > 1999)
+                throw new ArgumentOutOfRangeException(nameof(value), value, "must be in the 90s");
+        }
+    }
+
+    [The90s, Weekend] partial record SomeWeekendInThe90s;
+}
