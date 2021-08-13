d6d6dc8f73cd4854d306ac8d41a46364509c9cb5
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-13 14:10:07 +0200

    FSharp

--- src/AD.BaseTypes.FSharp/BaseType.fs
+++ src/AD.BaseTypes.FSharp/BaseType.fs
@@ -2,8 +2,12 @@
 
 open System
 
+/// Module for base types.
 module BaseType =
 
+    /// Creates a base type.
+    /// <param name="value">The base type's value.</param>
+    /// <returns>The created base type or an error message.</returns>
     let inline create<'baseType, 'wrapped when 'baseType :> IBaseType<'wrapped> and 'baseType : (static member Create: 'wrapped -> 'baseType)> value =
         try
             (^baseType : (static member Create: 'wrapped -> 'baseType) value) |> Ok
