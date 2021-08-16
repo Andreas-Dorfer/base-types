e85b87b9fe124c9a2202f05a2b494f8351179725
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-16 10:57:28 +0200

    refactoring

--- src/AD.BaseTypes.ModelBinders/BaseTypeModelBinderProvider.cs
+++ src/AD.BaseTypes.ModelBinders/BaseTypeModelBinderProvider.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using System;
+using System.Reflection;
 
 namespace AD.BaseTypes.ModelBinders
 {
@@ -8,11 +10,23 @@ namespace AD.BaseTypes.ModelBinders
         public IModelBinder GetBinder(ModelBinderProviderContext context)
         {
             var baseType = context.Metadata.ModelType;
-            var iBaseType = baseType.GetInterface(typeof(IBaseType<>).Name);
+            var iBaseType = GetBaseTypeInterface(baseType);
             if (iBaseType is null) return null!;
 
             var wrapped = iBaseType.GenericTypeArguments[0];
             return new BinderTypeModelBinder(typeof(BaseTypeModelBinder<,>).MakeGenericType(baseType, wrapped));
+        }
+
+        static Type? GetBaseTypeInterface(Type baseType)
+        {
+            try
+            {
+                return baseType.GetInterface(typeof(IBaseType<>).Name);
+            }
+            catch (AmbiguousMatchException)
+            {
+                return default;
+            }
         }
     }
 }
