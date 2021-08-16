4ba4b62d1177b61cffc74687d0d721a8e15f050d
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-16 11:39:55 +0200

    tests

--- src/AD.BaseTypes.Tests/BaseTypeTest.cs
+++ src/AD.BaseTypes.Tests/BaseTypeTest.cs
@@ -1,38 +1,9 @@
 ﻿using FsCheck;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.ComponentModel;
 
 namespace AD.BaseTypes.Tests
 {
-    public abstract class BaseTypeTest<TBaseType, TWrapped> where TBaseType : IBaseType<TWrapped>
+    public abstract class BaseTypeTest<TBaseType, TWrapped> : ValidatedBaseTypeTest<TBaseType, TWrapped> where TBaseType : IBaseType<TWrapped>
     {
-        protected TypeConverter Converter => TypeDescriptor.GetConverter(typeof(TBaseType));
-        protected TypeConverter WrappedConverter => TypeDescriptor.GetConverter(typeof(TWrapped));
-
-        protected abstract TBaseType New(TWrapped value);
-
-        [TestMethod]
-        public void Create() =>
-            Prop.ForAll<TWrapped>(value => Equals(value, New(value).Value)).QuickCheckThrowOnFailure();
-
-        [TestMethod]
-        public void ConvertFrom() =>
-            Prop.ForAll<TWrapped>(value => Equals(value, ((TBaseType)Converter.ConvertFrom(value)).Value)).QuickCheckThrowOnFailure();
-
-        [TestMethod]
-        public void ConvertTo() =>
-            Prop.ForAll<TWrapped>(value => Equals(value, Converter.ConvertTo(New(value), typeof(TWrapped)))).QuickCheckThrowOnFailure();
-
-        [TestMethod]
-        public void ConvertFromString() =>
-            Prop.ForAll<TWrapped>(value =>
-            {
-                var @string = WrappedConverter.ConvertToString(value);
-                return Equals(WrappedConverter.ConvertFromString(@string), ((TBaseType)Converter.ConvertFromString(@string)).Value);
-            }).QuickCheckThrowOnFailure();
-
-        [TestMethod]
-        public void ConvertToString() =>
-            Prop.ForAll<TWrapped>(value => WrappedConverter.ConvertToString(value) == Converter.ConvertToString(New(value))).QuickCheckThrowOnFailure();
+        protected override Arbitrary<TWrapped> Arbitrary => Arb.From<TWrapped>();
     }
 }
---
+++ src/AD.BaseTypes.Tests/ValidatedBaseTypeTest.cs
@@ -0,0 +1,39 @@
+﻿using FsCheck;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ComponentModel;
+
+namespace AD.BaseTypes.Tests
+{
+    public abstract class ValidatedBaseTypeTest<TBaseType, TWrapped> where TBaseType : IBaseType<TWrapped>
+    {
+        protected abstract TBaseType New(TWrapped value);
+        protected abstract Arbitrary<TWrapped> Arbitrary { get; }
+
+        protected TypeConverter Converter => TypeDescriptor.GetConverter(typeof(TBaseType));
+        protected TypeConverter WrappedConverter => TypeDescriptor.GetConverter(typeof(TWrapped));
+
+        [TestMethod]
+        public void Create() =>
+            Prop.ForAll(Arbitrary, value => Equals(value, New(value).Value)).QuickCheckThrowOnFailure();
+
+        [TestMethod]
+        public void ConvertFrom() =>
+            Prop.ForAll(Arbitrary, value => Equals(value, ((TBaseType)Converter.ConvertFrom(value)).Value)).QuickCheckThrowOnFailure();
+
+        [TestMethod]
+        public void ConvertTo() =>
+            Prop.ForAll(Arbitrary, value => Equals(value, Converter.ConvertTo(New(value), typeof(TWrapped)))).QuickCheckThrowOnFailure();
+
+        [TestMethod]
+        public void ConvertFromString() =>
+            Prop.ForAll(Arbitrary, value =>
+            {
+                var @string = WrappedConverter.ConvertToString(value);
+                return Equals(WrappedConverter.ConvertFromString(@string), ((TBaseType)Converter.ConvertFromString(@string)).Value);
+            }).QuickCheckThrowOnFailure();
+
+        [TestMethod]
+        public void ConvertToString() =>
+            Prop.ForAll(Arbitrary, value => WrappedConverter.ConvertToString(value) == Converter.ConvertToString(New(value))).QuickCheckThrowOnFailure();
+    }
+}
