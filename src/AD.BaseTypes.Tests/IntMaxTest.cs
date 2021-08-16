652490364da32bf994419e2d018e4986a98d076b
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-16 17:44:15 +0200

    IntMax tests

---
+++ src/AD.BaseTypes.Tests/IntMaxTest.cs
@@ -0,0 +1,24 @@
+﻿using FsCheck;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace AD.BaseTypes.Tests
+{
+    [IntMax(Max)]
+    public partial record MyIntMax
+    {
+        public const int Max = 100;
+    }
+
+    [TestClass]
+    public class IntMaxTest : ValidatedBaseTypeTest<MyIntMax, int>
+    {
+        protected override Arbitrary<int> Arbitrary => Arb.From(Gen.Choose(int.MinValue, MyIntMax.Max), value => Arb.Shrink(value).Where(_ => _ <= MyIntMax.Max));
+
+        protected override MyIntMax New(int value) => new(value);
+
+        [TestMethod, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void GreaterThanMax() => new MyIntMax(MyIntMax.Max + 1);
+    }
+}
