87274d75d7ae13946e0deb0aab9ee4be4d6c9844
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-16 14:14:13 +0200

    tests

---
+++ src/AD.BaseTypes.Tests/GuidTest.cs
@@ -0,0 +1,16 @@
+﻿using FsCheck;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AD.BaseTypes.Tests
+{
+    [Guid] public partial record MyGuid;
+
+    [TestClass]
+    public class GuidTest : ValidatedBaseTypeTest<MyGuid, Guid>
+    {
+        protected override Arbitrary<Guid> Arbitrary => Arb.Default.Guid().Filter(guid => guid != Guid.Empty);
+
+        protected override MyGuid New(Guid value) => new(value);
+    }
+}
