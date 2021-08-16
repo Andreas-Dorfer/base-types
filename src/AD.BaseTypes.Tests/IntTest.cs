9f067bcae4d28933f4f9023d158bf96c3d0ffa2e
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-16 11:22:06 +0200

    tests

---
+++ src/AD.BaseTypes.Tests/IntTest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AD.BaseTypes.Tests
+{
+    [Int] public partial record MyInt;
+
+    [TestClass]
+    public class IntTest : BaseTypeTest<MyInt, int>
+    {
+        protected override MyInt New(int value) => new(value);
+    }
+}
