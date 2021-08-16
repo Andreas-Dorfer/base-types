55e5c7f6b7b053694cc647aa63b2548c6030c0a8
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-16 09:02:29 +0200

    core

--- src/AD.BaseTypes.Core/BaseTypeConverter.cs
+++ src/AD.BaseTypes.Core/BaseTypeConverter.cs
@@ -4,18 +4,25 @@ using System.Globalization;
 
 namespace AD.BaseTypes.Core
 {
+    /// <inheritdoc/>
     public class BaseTypeConverter<TBaseType, TWrapped> : TypeConverter where TBaseType : IBaseType<TWrapped>
     {
+        readonly TypeConverter wrappedConverter = TypeDescriptor.GetConverter(typeof(TWrapped));
+
+        /// <inheritdoc/>
         public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType) =>
-            sourceType == typeof(TWrapped);
+            wrappedConverter.CanConvertFrom(context, sourceType);
 
+        /// <inheritdoc/>
         public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType) =>
-            destinationType == typeof(TWrapped);
+            wrappedConverter.CanConvertTo(context, destinationType);
 
+        /// <inheritdoc/>
         public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value) =>
-            BaseType<TBaseType, TWrapped>.Create((TWrapped)value);
+            BaseType<TBaseType, TWrapped>.Create((TWrapped)wrappedConverter.ConvertFrom(context, culture, value));
 
+        /// <inheritdoc/>
         public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType) =>
-            ((IBaseType<TWrapped>)value).Value!;
+            wrappedConverter.ConvertTo(context, culture, ((IBaseType<TWrapped>)value).Value, destinationType);
     }
 }
