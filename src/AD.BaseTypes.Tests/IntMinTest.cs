0fedc81453871e06d92c648dd981e07e97397e6f
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-16 17:50:13 +0200

    IntMin tests

---
+++ src/AD.BaseTypes.Tests/IntMinTest.cs
@@ -0,0 +1,24 @@
+﻿using FsCheck;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace AD.BaseTypes.Tests
+{
+    [IntMin(Min)]
+    public partial record MyIntMin
+    {
+        public const int Min = -100;
+    }
+
+    [TestClass]
+    public class IntMinTest : ValidatedBaseTypeTest<MyIntMin, int>
+    {
+        protected override Arbitrary<int> Arbitrary => Arb.From(Gen.Choose(MyIntMin.Min, int.MaxValue), value => Arb.Shrink(value).Where(_ => _ >= MyIntMin.Min));
+
+        protected override MyIntMin New(int value) => new(value);
+
+        [TestMethod, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void SmallerThanMin() => new MyIntMin(MyIntMin.Min - 1);
+    }
+}
