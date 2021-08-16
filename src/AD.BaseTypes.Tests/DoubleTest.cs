95f1839f2a5baf81f20b558c52d1fa136fe66373
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-16 11:20:28 +0200

    tests

---
+++ src/AD.BaseTypes.Tests/DoubleTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AD.BaseTypes.Tests
+{
+    [Double] public partial record MyDouble;
+
+    [TestClass]
+    public class DoubleTest : BaseTypeTest<MyDouble, double>
+    {
+        protected override MyDouble New(double value) => new(value);
+    }
+}
