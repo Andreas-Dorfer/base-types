b48d4efaad975f3b3509aecc4c96692b9486d129
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-16 20:10:24 +0200

    IntRange test

---
+++ src/AD.BaseTypes.Tests/IntRangeTest.cs
@@ -0,0 +1,20 @@
+﻿using FsCheck;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace AD.BaseTypes.Tests
+{
+    [IntRange(Min, Max)]
+    public partial record MyIntRange
+    {
+        public const int Min = -100, Max = 100;
+    }
+
+    [TestClass]
+    public class IntRangeTest : ValidatedBaseTypeTest<MyIntRange, int>
+    {
+        protected override Arbitrary<int> Arbitrary => Arb.From(Gen.Choose(MyIntRange.Min, MyIntRange.Max), value => Arb.Shrink(value).Where(_ => _ >= MyIntRange.Min && _ <= MyIntRange.Max));
+
+        protected override MyIntRange New(int value) => new(value);
+    }
+}
