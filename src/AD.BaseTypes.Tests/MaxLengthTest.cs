d2c03160f673227b4364479dd21d1ac45fc9405e
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-18 19:29:27 +0200

    MaxLength test

---
+++ src/AD.BaseTypes.Tests/MaxLengthTest.cs
@@ -0,0 +1,18 @@
+﻿using AD.BaseTypes.Arbitraries;
+using FsCheck;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AD.BaseTypes.Tests
+{
+    [MaxLength(MaxLength)]
+    public partial record MyMaxLength
+    {
+        public const int MaxLength = 10;
+    }
+
+    [TestClass]
+    public class MaxLengthTest : BaseTypeTest<MyMaxLength, string>
+    {
+        protected override Arbitrary<MyMaxLength> Arbitrary => new MaxLengthArbitrary<MyMaxLength>(MyMaxLength.MaxLength);
+    }
+}
