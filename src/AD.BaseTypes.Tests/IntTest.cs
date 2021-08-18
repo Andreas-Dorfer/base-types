c58f4cc24a4652e9393061fb5a53dec5714a8603
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-18 18:39:10 +0200

    int test

--- src/AD.BaseTypes.Tests/IntTest.cs
+++ src/AD.BaseTypes.Tests/IntTest.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using AD.BaseTypes.Arbitraries;
+using FsCheck;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AD.BaseTypes.Tests
 {
     [Int] public partial record MyInt;
 
-    //[TestClass]
-    //public class IntTest : BaseTypeTest<MyInt, int>
-    //{
-    //    protected override MyInt New(int value) => new(value);
-    //}
+    [TestClass]
+    public class IntTest : BaseTypeTest<MyInt, int>
+    {
+        protected override Arbitrary<MyInt> Arbitrary => new IntArbitrary<MyInt>();
+    }
 }
