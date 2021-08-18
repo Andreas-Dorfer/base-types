4f81fb9c180e6d717562345c49ffa865fb7186ce
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-18 19:51:09 +0200

    PositiveDecimal test

--- src/AD.BaseTypes.Arbitraries/DecimalArbitrary.cs
+++ src/AD.BaseTypes.Arbitraries/DecimalArbitrary.cs
@@ -1,9 +1,16 @@
-﻿namespace AD.BaseTypes.Arbitraries
+﻿using FsCheck;
+
+namespace AD.BaseTypes.Arbitraries
 {
     /// <summary>
     /// Arbitrary for decimal base types.
     /// </summary>
     /// <typeparam name="TBaseType">The base type.</typeparam>
     public class DecimalArbitrary<TBaseType> : BaseTypeArbitrary<TBaseType, decimal> where TBaseType : IBaseType<decimal>
-    { }
+    {
+        /// <inheritdoc/>
+        public override Gen<TBaseType> Generator =>
+            //the default decimal generator doesn't generate negative values --> invert some values
+            Arb.Default.Decimal().Generator.Zip(Arb.Default.Bool().Generator, (value, invert) => invert ? -value : value).Where(Filter).Select(Creator);
+    }
 }
---
+++ src/AD.BaseTypes.Tests/PositiveDecimalTest.cs
@@ -0,0 +1,14 @@
+﻿using AD.BaseTypes.Arbitraries;
+using FsCheck;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AD.BaseTypes.Tests
+{
+    [PositiveDecimal] public partial record MyPositiveDecimal;
+
+    [TestClass]
+    public class PositiveDecimalTest : BaseTypeTest<MyPositiveDecimal, decimal>
+    {
+        protected override Arbitrary<MyPositiveDecimal> Arbitrary => new PositiveDecimalArbitrary<MyPositiveDecimal>();
+    }
+}
