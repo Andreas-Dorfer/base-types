146532b4c29be8392659f9474c8120ad57acae77
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-18 20:19:16 +0200

    Regex test

---
+++ src/AD.BaseTypes.Arbitraries/ExampleArbitrary.cs
@@ -0,0 +1,28 @@
+﻿using FsCheck;
+using System;
+
+namespace AD.BaseTypes.Arbitraries
+{
+    /// <summary>
+    /// Example based arbitrary.
+    /// </summary>
+    /// <typeparam name="TBaseType">The base type.</typeparam>
+    /// <typeparam name="TWrapped">The wrapped type.</typeparam>
+    public class ExampleArbitrary<TBaseType, TWrapped> : Arbitrary<TBaseType> where TBaseType : IBaseType<TWrapped>
+    {
+        readonly TWrapped[] examples;
+
+        /// <summary>
+        /// Creates the arbitrary.
+        /// </summary>
+        /// <param name="examples">The examples to use.</param>
+        /// <exception cref="ArgumentNullException">The parameter <paramref name="examples"/> is null.</exception>
+        public ExampleArbitrary(params TWrapped[] examples)
+        {
+            this.examples = examples ?? throw new ArgumentNullException(nameof(examples));
+        }
+
+        /// <inheritdoc/>
+        public override Gen<TBaseType> Generator => Gen.Elements(examples).Select(BaseType<TBaseType, TWrapped>.Create);
+    }
+}
---
+++ src/AD.BaseTypes.Tests/RegexTest.cs
@@ -0,0 +1,14 @@
+﻿using AD.BaseTypes.Arbitraries;
+using FsCheck;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AD.BaseTypes.Tests
+{
+    [Regex(@"\d\d\w\w")] public partial record MyRegex;
+
+    [TestClass]
+    public class RegexTest : BaseTypeTest<MyRegex, string>
+    {
+        protected override Arbitrary<MyRegex> Arbitrary => new ExampleArbitrary<MyRegex, string>("00aa", "11bb", "42Ek", "72mI", "99XX");
+    }
+}
