322d90f7b3c19a720728d5f2916221b9a3f92cdb
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-18 19:03:49 +0200

    IntMax test

--- src/AD.BaseTypes.Arbitraries/IntMaxArbitrary.cs
+++ src/AD.BaseTypes.Arbitraries/IntMaxArbitrary.cs
@@ -1,13 +1,37 @@
-﻿namespace AD.BaseTypes.Arbitraries
+﻿using FsCheck;
+using System;
+
+namespace AD.BaseTypes.Arbitraries
 {
     /// <summary>
     /// Arbitrary for int base types with a maximal value.
     /// </summary>
     /// <typeparam name="TBaseType"></typeparam>
-    public class IntMaxArbitrary<TBaseType> : IntRangeArbitrary<TBaseType> where TBaseType : IBaseType<int>
+    public class IntMaxArbitrary<TBaseType> : IntArbitrary<TBaseType> where TBaseType : IBaseType<int>
     {
         /// <param name="max">The maximal value.</param>
-        public IntMaxArbitrary(int max) : base(int.MinValue, max)
-        { }
+        public IntMaxArbitrary(int max)
+        {
+            Max = max;
+        }
+
+        /// <summary>
+        /// The maximal value.
+        /// </summary>
+        protected int Max { get; }
+
+        /// <summary>
+        /// Filters too large values.
+        /// </summary>
+        /// <param name="value">The value to check.</param>
+        /// <returns>True, if the value is valid.</returns>
+        protected override bool Filter(int value) => value <= Max;
+
+        /// <inheritdoc/>
+        public override Gen<TBaseType> Generator => Gen.Sized(i =>
+        {
+            var maxSub = Max < 0 ? int.MaxValue + Max + 1 : int.MaxValue;
+            return Gen.Choose(Max - Math.Min(i, maxSub), Max);
+        }).Select(Creator);
     }
 }
--- src/AD.BaseTypes.Tests/IntMaxTest.cs
+++ src/AD.BaseTypes.Tests/IntMaxTest.cs
@@ -1,7 +1,6 @@
-﻿using FsCheck;
+﻿using AD.BaseTypes.Arbitraries;
+using FsCheck;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Linq;
 
 namespace AD.BaseTypes.Tests
 {
@@ -11,14 +10,9 @@ namespace AD.BaseTypes.Tests
         public const int Max = 100;
     }
 
-    //[TestClass]
-    //public class IntMaxTest : ValidatedBaseTypeTest<MyIntMax, int>
-    //{
-    //    protected override Arbitrary<int> Arbitrary => Arb.From(Gen.Choose(int.MinValue, MyIntMax.Max), value => Arb.Shrink(value).Where(_ => _ <= MyIntMax.Max));
-
-    //    protected override MyIntMax New(int value) => new(value);
-
-    //    [TestMethod, ExpectedException(typeof(ArgumentOutOfRangeException))]
-    //    public void MoreThanMax() => new MyIntMax(MyIntMax.Max + 1);
-    //}
+    [TestClass]
+    public class IntMaxTest : BaseTypeTest<MyIntMax, int>
+    {
+        protected override Arbitrary<MyIntMax> Arbitrary => new IntMaxArbitrary<MyIntMax>(MyIntMax.Max);
+    }
 }
