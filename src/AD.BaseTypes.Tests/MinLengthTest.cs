92b5ff681d8368fd21cadeed30815f11f5a94a38
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-18 19:34:17 +0200

    MinLength test

---
+++ src/AD.BaseTypes.Tests/MinLengthTest.cs
@@ -0,0 +1,18 @@
+﻿using AD.BaseTypes.Arbitraries;
+using FsCheck;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AD.BaseTypes.Tests
+{
+    [MinLength(MinLength)]
+    public partial record MyMinLength
+    {
+        public const int MinLength = 10;
+    }
+
+    [TestClass]
+    public class MinLengthTest : BaseTypeTest<MyMinLength, string>
+    {
+        protected override Arbitrary<MyMinLength> Arbitrary => new MinLengthArbitrary<MyMinLength>(MyMinLength.MinLength);
+    }
+}
