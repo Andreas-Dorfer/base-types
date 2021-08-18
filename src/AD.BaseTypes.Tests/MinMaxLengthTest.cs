0535119e2ae88b58af4f895c3663e562e916d082
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-18 19:37:04 +0200

    MinMaxLength test

---
+++ src/AD.BaseTypes.Tests/MinMaxLengthTest.cs
@@ -0,0 +1,18 @@
+﻿using AD.BaseTypes.Arbitraries;
+using FsCheck;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AD.BaseTypes.Tests
+{
+    [MinMaxLength(MinLength, MaxLength)]
+    public partial record MyMinMaxLength
+    {
+        public const int MinLength = 10, MaxLength = 20;
+    }
+
+    [TestClass]
+    public class MinMaxLengthTest : BaseTypeTest<MyMinMaxLength, string>
+    {
+        protected override Arbitrary<MyMinMaxLength> Arbitrary => new MinMaxLengthArbitrary<MyMinMaxLength>(MyMinMaxLength.MinLength, MyMinMaxLength.MaxLength);
+    }
+}
