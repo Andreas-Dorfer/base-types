44f3f426a98af81cc903d17979e4899c782f4b46
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-18 19:39:42 +0200

    NonEmptyString test

---
+++ src/AD.BaseTypes.Tests/NonEmptyStringTest.cs
@@ -0,0 +1,14 @@
+﻿using AD.BaseTypes.Arbitraries;
+using FsCheck;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AD.BaseTypes.Tests
+{
+    [NonEmptyString] public partial record MyNonEmptyString;
+
+    [TestClass]
+    public class NonEmptyStringTest : BaseTypeTest<MyNonEmptyString, string>
+    {
+        protected override Arbitrary<MyNonEmptyString> Arbitrary => new NonEmptyStringArbitrary<MyNonEmptyString>();
+    }
+}
