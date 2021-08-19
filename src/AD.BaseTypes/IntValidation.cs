712bf2df77054a4bc06e0499b4ca4a01ea79debb
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-19 10:35:34 +0200

    ref

--- src/AD.BaseTypes/IntValidation.cs
+++ src/AD.BaseTypes/IntValidation.cs
@@ -6,12 +6,12 @@ namespace AD.BaseTypes
     {
         public static void Min(int min, int value)
         {
-            if (value < min) throw new ArgumentOutOfRangeException(nameof(value), value, $"Parameter must be more than {min}.");
+            if (value < min) throw new ArgumentOutOfRangeException(nameof(value), value, $"Parameter must be greater than or equal to {min}.");
         }
 
         public static void Max(int max, int value)
         {
-            if (value > max) throw new ArgumentOutOfRangeException(nameof(value), value, $"Parameter must be less than {max}.");
+            if (value > max) throw new ArgumentOutOfRangeException(nameof(value), value, $"Parameter must be less than or equal to {max}.");
         }
     }
 }
