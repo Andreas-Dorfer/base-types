be9568cbd7aa88984abb8ae3f5d80d2506b2e229
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-19 21:25:44 +0200

    map test

--- src/AD.BaseTypes.Core/Extensions/BaseTypeExtensions.cs
+++ src/AD.BaseTypes.Core/Extensions/BaseTypeExtensions.cs
@@ -50,13 +50,12 @@ namespace AD.BaseTypes.Extensions
         /// <summary>
         /// Maps a base type's value.
         /// </summary>
-        /// <typeparam name="TBaseType">The base type.</typeparam>
         /// <typeparam name="TWrapped">The wrapped type.</typeparam>
         /// <param name="baseType">The base type.</param>
         /// <param name="mapper">The mapper.</param>
         /// <returns>The mapped value.</returns>
         /// <exception cref="ArgumentNullException"><paramref name="baseType"/> or <paramref name="mapper"/> is null.</exception>
-        public static TWrapped MapValue<TBaseType, TWrapped>(this TBaseType baseType, Func<TWrapped, TWrapped> mapper) where TBaseType : IBaseType<TWrapped>
+        public static TWrapped MapValue<TWrapped>(this IBaseType<TWrapped> baseType, Func<TWrapped, TWrapped> mapper)
         {
             if (baseType is null) throw new ArgumentNullException(nameof(baseType));
             if (mapper is null) throw new ArgumentNullException(nameof(mapper));
--- src/AD.BaseTypes.Tests/MapTest.cs
+++ src/AD.BaseTypes.Tests/MapTest.cs
@@ -30,5 +30,8 @@ namespace AD.BaseTypes.Tests
 
         [TestMethod]
         public void TryError() => Assert.IsFalse(new ZeroToTen(ZeroToTen.Max).TryMap<ZeroToTen, int>(Add1, out var _, out var _));
+
+        [TestMethod]
+        public void MapValue() => Assert.AreEqual(Add1(ZeroToTen.Max), new ZeroToTen(ZeroToTen.Max).MapValue(Add1));
     }
 }
