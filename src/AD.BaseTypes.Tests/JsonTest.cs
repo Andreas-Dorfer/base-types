474bb838ed6c40f4b0c8e0f8cdcb6b640481ca38
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-19 21:29:44 +0200

    json test

---
+++ src/AD.BaseTypes.Tests/JsonTest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.Json;
+
+namespace AD.BaseTypes.Tests
+{
+    [TestClass]
+    public class JsonTest
+    {
+        [TestMethod, ExpectedException(typeof(JsonException))]
+        public void DeserializeError() => JsonSerializer.Deserialize<ZeroToTen>(JsonSerializer.Serialize(ZeroToTen.Max + 1));
+    }
+}
