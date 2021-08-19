bb9aedd704b53eb1609df78af1c6105eb8334ce1
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-19 21:10:37 +0200

    create tests

---
+++ src/AD.BaseTypes.Tests/CreateTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AD.BaseTypes.Tests
+{
+    [IntRange(Min, Max)]
+    public partial record ZeroToTen
+    {
+        public const int Min = 0, Max = 10;
+    }
+
+    [TestClass]
+    public class CreateTest
+    {
+        [TestMethod]
+        public void Ok() => BaseType<ZeroToTen, int>.Create(ZeroToTen.Max / 2);
+
+        [TestMethod, ExpectedException(typeof(ArgumentException), AllowDerivedTypes = true)]
+        public void Error() => BaseType<ZeroToTen, int>.Create(ZeroToTen.Max + 1);
+
+        [TestMethod]
+        public void TryOk() => Assert.IsTrue(BaseType<ZeroToTen, int>.TryCreate(ZeroToTen.Max / 2, out var _, out var _));
+
+        [TestMethod]
+        public void TryError() => Assert.IsFalse(BaseType<ZeroToTen, int>.TryCreate(ZeroToTen.Max + 1, out var _, out var _));
+    }
+}
