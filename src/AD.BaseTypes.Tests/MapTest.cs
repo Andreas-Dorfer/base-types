87020fb2fc727b5487a0abf03bb21ca092202d36
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-19 21:23:43 +0200

    map test

---
+++ src/AD.BaseTypes.Tests/MapTest.cs
@@ -0,0 +1,34 @@
+﻿using AD.BaseTypes.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AD.BaseTypes.Tests
+{
+    [TestClass]
+    public class MapTest
+    {
+        static int Add1(int i) => i + 1;
+
+        [TestMethod]
+        public void Ok() => Assert.AreEqual(Add1(ZeroToTen.Min), new ZeroToTen(ZeroToTen.Min).Map<ZeroToTen, int>(Add1));
+
+        [TestMethod, ExpectedException(typeof(ArgumentException), AllowDerivedTypes = true)]
+        public void Error() => new ZeroToTen(ZeroToTen.Max).Map<ZeroToTen, int>(Add1);
+
+        [TestMethod]
+        public void TryMapOk()
+        {
+            if (new ZeroToTen(ZeroToTen.Min).TryMap<ZeroToTen, int>(Add1, out var baseType, out var _))
+            {
+                Assert.AreEqual(Add1(ZeroToTen.Min), baseType);
+            }
+            else
+            {
+                Assert.Fail("true expected");
+            }
+        }
+
+        [TestMethod]
+        public void TryError() => Assert.IsFalse(new ZeroToTen(ZeroToTen.Max).TryMap<ZeroToTen, int>(Add1, out var _, out var _));
+    }
+}
