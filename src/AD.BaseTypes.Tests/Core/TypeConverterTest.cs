a877059bbb4fdef16acad72b512cd2359771b8f4
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-08-20 10:33:45 +0200

    tests

--- src/AD.BaseTypes.Tests/Core/TypeConverterTest.cs
+++ src/AD.BaseTypes.Tests/Core/TypeConverterTest.cs
@@ -14,5 +14,17 @@ namespace AD.BaseTypes.Tests.Core
 
         [TestMethod, ExpectedException(typeof(ArgumentException), AllowDerivedTypes = true)]
         public void InvalidConvertFromString() => converter.ConvertFromString((ZeroToTen.Max + 1).ToString());
+
+        [TestMethod]
+        public void NotSupportedCanConvertFrom() => Assert.IsFalse(converter.CanConvertFrom(typeof(double)));
+
+        [TestMethod, ExpectedException(typeof(NotSupportedException))]
+        public void NotSupportedConvertFrom() => converter.ConvertFrom(5.0);
+
+        [TestMethod]
+        public void NotSupportedCanConvertTo() => Assert.IsFalse(converter.CanConvertTo(typeof(DateTime)));
+
+        [TestMethod, ExpectedException(typeof(NotSupportedException))]
+        public void NotSupportedConvertTo() => converter.ConvertTo(new ZeroToTen(ZeroToTen.Max / 2), typeof(DateTime));
     }
 }
