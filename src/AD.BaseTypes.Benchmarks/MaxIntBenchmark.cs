5d81be163ba666b9cfb561841a703040e00a0108
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2022-11-30 12:41:58 +0100

    static MaxInt validation

---
+++ src/AD.BaseTypes.Benchmarks/MaxIntBenchmark.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace AD.BaseTypes.Benchmarks;
+
+[MaxInt(Max)]
+public partial record StaticMaxInt
+{
+    public const int Max = 100;
+}
+
+[AttributeUsage(AttributeTargets.Class)]
+public class InstanceMaxIntAttribute : Attribute, IBaseTypeValidation<int>
+{
+    readonly int max;
+
+    public InstanceMaxIntAttribute(int max)
+    {
+        this.max = max;
+    }
+
+    public void Validate(int value) => IntValidation.Max(max, value);
+}
+
+[InstanceMaxInt(StaticMaxInt.Max)] public partial record InstanceMaxInt;
+
+[MemoryDiagnoser]
+public class MaxIntBenchmark
+{
+    const int value = StaticMaxInt.Max / 2;
+
+#pragma warning disable CA1822 // Mark members as static
+#pragma warning disable CA1806 // Do not ignore method results
+    [Benchmark]
+    public void WithInstanceMethod() => new InstanceMaxInt(value);
+
+    [Benchmark]
+    public void WithStaticMethod() => new StaticMaxInt(value);
+#pragma warning restore CA1822 // Mark members as static
+#pragma warning restore CA1806 // Do not ignore method results
+}
