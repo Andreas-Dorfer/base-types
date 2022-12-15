1ef1b79042436f88b2f3e7795340a143f652ef79
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2022-12-15 11:22:32 +0100

    MinMaxLengthString

---
+++ src/AD.BaseTypes.Benchmarks/MinMaxLengthStringBenchmark.cs
@@ -0,0 +1,43 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace AD.BaseTypes.Benchmarks;
+
+[MinMaxLengthString(MinLength, MaxLength)]
+public sealed partial record StaticMinMaxLengthString
+{
+    public const int MinLength = 5, MaxLength = 15;
+}
+
+[AttributeUsage(AttributeTargets.Class)]
+public class InstanceMinMaxLengthStringAttribute : Attribute, IBaseTypeValidation<string>
+{
+    readonly int minLength, maxLength;
+
+    public InstanceMinMaxLengthStringAttribute(int minLength, int maxLength)
+    {
+        this.minLength = minLength;
+        this.maxLength = maxLength;
+    }
+
+    public void Validate(string value)
+    {
+        StringValidation.MinLength(minLength, value);
+        StringValidation.MaxLength(maxLength, value);
+    }
+}
+
+[InstanceMinMaxLengthString(StaticMinLengthString.MinLength, StaticMaxLengthString.MaxLength)] public sealed partial record InstanceMinMaxLengthString;
+
+[MemoryDiagnoser]
+public class MinMaxLengthStringBenchmark
+{
+    readonly string value = "teststring";
+
+#pragma warning disable CA1806 // Do not ignore method results
+    [Benchmark]
+    public void WithInstanceMethod() => new InstanceMinMaxLengthString(value);
+
+    [Benchmark]
+    public void WithStaticMethod() => new StaticMinMaxLengthString(value);
+#pragma warning restore CA1806 // Do not ignore method results
+}
--- src/AD.BaseTypes/MinMaxLengthStringAttribute.cs
+++ src/AD.BaseTypes/MinMaxLengthStringAttribute.cs
@@ -4,20 +4,20 @@
 /// String with a minimal and maximal length.
 /// </summary>
 [AttributeUsage(AttributeTargets.Class)]
-public class MinMaxLengthStringAttribute : Attribute, IBaseTypeValidation<string>
+public sealed class MinMaxLengthStringAttribute : Attribute, IStaticBaseTypeValidation<string>
 {
-    readonly int minLength, maxLength;
-
+#pragma warning disable IDE0060 // Remove unused parameter
     /// <param name="minLength">Minimal length.</param>
     /// <param name="maxLength">Maximal length.</param>
     public MinMaxLengthStringAttribute(int minLength, int maxLength)
-    {
-        this.minLength = minLength;
-        this.maxLength = maxLength;
-    }
+    { }
+#pragma warning restore IDE0060 // Remove unused parameter
 
+    /// <param name="value">The value to be validated.</param>
+    /// <param name="minLength">Minimal length.</param>
+    /// <param name="maxLength">Maximal length.</param>
     /// <exception cref="ArgumentOutOfRangeException">The parameter <paramref name="value"/> is too short or too long.</exception>
-    public void Validate(string value)
+    public static void Validate(string value, int minLength, int maxLength)
     {
         StringValidation.MinLength(minLength, value);
         StringValidation.MaxLength(maxLength, value);
