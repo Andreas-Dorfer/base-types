6a656539edd21c538f5701bf41aab3bf40b21be5
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2022-12-15 11:31:49 +0100

    NonEmptyString

--- src/AD.BaseTypes/NonEmptyStringAttribute.cs
+++ src/AD.BaseTypes/NonEmptyStringAttribute.cs
@@ -4,10 +4,11 @@
 /// Non-empty string.
 /// </summary>
 [AttributeUsage(AttributeTargets.Class)]
-public class NonEmptyStringAttribute : Attribute, IBaseTypeValidation<string>
+public sealed class NonEmptyStringAttribute : Attribute, IStaticBaseTypeValidation<string>
 {
+    /// <param name="value">The value to be validated.</param>
     /// <exception cref="ArgumentOutOfRangeException">The parameter <paramref name="value"/> is empty.</exception>
-    public void Validate(string value)
+    public static void Validate(string value)
     {
         ArgumentNullException.ThrowIfNull(value);
         if (value == string.Empty) throw new ArgumentOutOfRangeException(nameof(value), value, "Parameter must not be empty.");
