98dacd3a86b2cac8f6f35b8d23ba5d0166d3586b
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2022-12-15 11:35:50 +0100

    PositiveDecimal

---
+++ src/AD.BaseTypes.Benchmarks/PositiveDecimalBenchmark.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace AD.BaseTypes.Benchmarks;
+
+[PositiveDecimal] public sealed partial record StaticPositiveDecimal;
+
+[AttributeUsage(AttributeTargets.Class)]
+public sealed class InstancePositiveDecimalAttribute : Attribute, IBaseTypeValidation<decimal>
+{
+    /// <exception cref="ArgumentOutOfRangeException">The parameter <paramref name="value"/> is negative.</exception>
+    public void Validate(decimal value)
+    {
+        if (value < 0) throw new ArgumentOutOfRangeException(nameof(value), value, "Parameter must not be negative.");
+    }
+}
+
+[InstancePositiveDecimal] public sealed partial record InstancePositiveDecimal;
+
+[MemoryDiagnoser]
+public class PositiveDecimalBenchmark
+{
+    readonly decimal value = 42;
+
+#pragma warning disable CA1806 // Do not ignore method results
+    [Benchmark]
+    public void WithInstanceMethod() => new InstancePositiveDecimal(value);
+
+    [Benchmark]
+    public void WithStaticMethod() => new StaticPositiveDecimal(value);
+#pragma warning restore CA1806 // Do not ignore method results
+}
--- src/AD.BaseTypes/PositiveDecimalAttribute.cs
+++ src/AD.BaseTypes/PositiveDecimalAttribute.cs
@@ -4,10 +4,11 @@
 /// Positive Decimal.
 /// </summary>
 [AttributeUsage(AttributeTargets.Class)]
-public class PositiveDecimalAttribute : Attribute, IBaseTypeValidation<decimal>
+public sealed class PositiveDecimalAttribute : Attribute, IStaticBaseTypeValidation<decimal>
 {
+    /// <param name="value">The value to be validated.</param>
     /// <exception cref="ArgumentOutOfRangeException">The parameter <paramref name="value"/> is negative.</exception>
-    public void Validate(decimal value)
+    public static void Validate(decimal value)
     {
         if (value < 0) throw new ArgumentOutOfRangeException(nameof(value), value, "Parameter must not be negative.");
     }
