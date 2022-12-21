96758cc569948222d60a32b760500ae87b6276ca
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2022-12-21 14:44:14 +0100

    < NET8_0 fix

--- src/AD.BaseTypes.Core/Json/BaseTypeJsonConverter.cs
+++ src/AD.BaseTypes.Core/Json/BaseTypeJsonConverter.cs
@@ -8,7 +8,7 @@ namespace AD.BaseTypes.Json;
 /// </summary>
 /// <typeparam name="TBaseType">The base type.</typeparam>
 /// <typeparam name="TWrapped">The wrapped type.</typeparam>
-public class BaseTypeJsonConverter<TBaseType, TWrapped> : JsonConverter<TBaseType> where TBaseType : IBaseType<TBaseType, TWrapped> where TWrapped : notnull
+public sealed class BaseTypeJsonConverter<TBaseType, TWrapped> : JsonConverter<TBaseType> where TBaseType : IBaseType<TBaseType, TWrapped> where TWrapped : notnull
 {
     static readonly Type WrappedType = typeof(TWrapped);
 
@@ -23,6 +23,11 @@ public class BaseTypeJsonConverter<TBaseType, TWrapped> : JsonConverter<TBaseTyp
         return value is null ? default : From(value);
     }
 
+
+    /* ************************************************
+     * see https://github.com/dotnet/runtime/pull/77488
+     * ************************************************ */
+#if NET8_0_OR_GREATER
     /// <inheritdoc/>
     public override void WriteAsPropertyName(Utf8JsonWriter writer, TBaseType baseType, JsonSerializerOptions options) =>
         GetConverter(options).WriteAsPropertyName(writer, baseType.Value, options);
@@ -30,6 +35,25 @@ public class BaseTypeJsonConverter<TBaseType, TWrapped> : JsonConverter<TBaseTyp
     /// <inheritdoc/>
     public override TBaseType ReadAsPropertyName(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
         From(GetConverter(options).ReadAsPropertyName(ref reader, WrappedType, options));
+#warning remove else
+#else
+    static readonly DummyConverter PropertyConverter = new();
+
+    sealed class DummyConverter : JsonConverter<TWrapped>
+    {
+        public override TWrapped? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) => throw new NotImplementedException();
+        public override void Write(Utf8JsonWriter writer, TWrapped value, JsonSerializerOptions options) => throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public override void WriteAsPropertyName(Utf8JsonWriter writer, TBaseType baseType, JsonSerializerOptions options) =>
+        PropertyConverter.WriteAsPropertyName(writer, baseType.Value, options);
+
+    /// <inheritdoc/>
+    public override TBaseType ReadAsPropertyName(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+        From(PropertyConverter.ReadAsPropertyName(ref reader, WrappedType, options));
+#endif
+    /* ************************************************ */
 
 
     static JsonConverter<TWrapped> GetConverter(JsonSerializerOptions options) =>
