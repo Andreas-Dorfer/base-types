d5ed6ed8ff7a8dc3e3b2576e5ea4b92d0f6a1fa3
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2022-12-21 12:42:00 +0100

    Read/WriteAsPropertyName

--- src/AD.BaseTypes.Core/Json/BaseTypeJsonConverter.cs
+++ src/AD.BaseTypes.Core/Json/BaseTypeJsonConverter.cs
@@ -10,15 +10,31 @@ namespace AD.BaseTypes.Json;
 /// <typeparam name="TWrapped">The wrapped type.</typeparam>
 public class BaseTypeJsonConverter<TBaseType, TWrapped> : JsonConverter<TBaseType> where TBaseType : IBaseType<TBaseType, TWrapped> where TWrapped : notnull
 {
+    static readonly Type WrappedType = typeof(TWrapped);
+
     /// <inheritdoc/>
     public override void Write(Utf8JsonWriter writer, TBaseType baseType, JsonSerializerOptions options) =>
-        JsonSerializer.Serialize(writer, baseType.Value, options);
+        GetConverter(options).Write(writer, baseType.Value, options);
 
     /// <inheritdoc/>
     public override TBaseType? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        var value = JsonSerializer.Deserialize<TWrapped>(ref reader, options);
-        return value is null ? default :
-            BaseType<TBaseType, TWrapped>.TryFrom(value, out var baseType, out var errorMessage) ? baseType : throw new JsonException(errorMessage);
+        var value = GetConverter(options).Read(ref reader, WrappedType, options);
+        return value is null ? default : From(value);
     }
+
+    /// <inheritdoc/>
+    public override void WriteAsPropertyName(Utf8JsonWriter writer, TBaseType baseType, JsonSerializerOptions options) =>
+        GetConverter(options).WriteAsPropertyName(writer, baseType.Value, options);
+
+    /// <inheritdoc/>
+    public override TBaseType ReadAsPropertyName(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+        From(GetConverter(options).ReadAsPropertyName(ref reader, WrappedType, options));
+
+
+    static JsonConverter<TWrapped> GetConverter(JsonSerializerOptions options) =>
+        options.GetConverter(WrappedType) as JsonConverter<TWrapped> ?? throw new NotSupportedException($"There is no converter for type '{WrappedType}'.");
+
+    static TBaseType From(TWrapped value) =>
+        BaseType<TBaseType, TWrapped>.TryFrom(value, out var baseType, out var errorMessage) ? baseType : throw new JsonException(errorMessage);
 }
